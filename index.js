52974fdf501f86ae96746d94220e6aa37c9acfe5
Henok <henokmicrometre@gmail.com>
2023-09-01 12:43:19 +0100

    added post

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 const app = express();
-
-
-
-
-
-
-
-
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,15 +10,17 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public', 'css')));
 
 
-
+app.use(bodyParser.json({extended : true}));
 app.post('/anpr', (req, res) => {
+ 	var plate = req.body.results[0].plate;
+  console.log(plate)
   res.send('POST request to the homepage')
 })
 
 
 app.get('/video', (req, res) => {
 
-  res.sendFile('alprVideo.mp4', { root: 'public/uploads'});
+  res.sendFile('alprVideo.mp4', { root: 'public/uploads' });
 });
 
 app.listen(5000, () => console.log('listening on port 5000.'));
