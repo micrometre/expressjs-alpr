044801bb87fd64aa2d42c41bf057d557435edfa2
Henok <henokmicrometre@gmail.com>
2023-09-01 13:21:39 +0100

    adding socket

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
-
+const fs = require('fs');
 const app = express();
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public', 'css')));
-
-
 app.use(bodyParser.json({extended : true}));
+
+
 app.post('/anpr', (req, res) => {
- 	var plate = req.body.results[0].plate;
+ 	let plate = req.body.results[0].plate;
   console.log(plate)
-  res.send('POST request to the homepage')
+  res.send(plate)
 })
 
 
-app.get('/video', (req, res) => {
 
+app.get('/video', (req, res) => {
   res.sendFile('alprVideo.mp4', { root: 'public/uploads' });
 });
+
 
 app.listen(5000, () => console.log('listening on port 5000.'));
