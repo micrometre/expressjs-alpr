e13a2731481031ef0d0d94239b24681d36285f20
Your Name <henokmicrometre@gmail.com>
2023-09-01 16:22:01 +0000

    added app.locals

--- index.js
+++ index.js
@@ -7,23 +7,33 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public', 'css')));
-app.use(bodyParser.json({extended : true}));
+app.use(bodyParser.json({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  console.log(app.locals.alpr_data)
+  res.send('ALPR/ANPR')
+
+})
+
 
 
 app.post('/anpr', (req, res) => {
- 	let plate = req.body.results[0].plate;
- 	let confidence= req.body.results[0].confidence;
- 	let uuid = req.body.uuid;
-  let data = [plate, confidence,  uuid]; 
-  console.log(uuid, plate, confidence,)
-  console.log(typeof(data))
+  let plate = req.body.results[0].plate;
+  let confidence = req.body.results[0].confidence;
+  let uuid = req.body.uuid;
+  let data = [plate, confidence, uuid];
+  app.locals.alpr_data = data
+  console.log(app.locals.alpr_data)
   res.send(plate)
 })
 
 
 
 app.get('/video', (req, res) => {
+  console.log(app.locals.alpr_data)
   res.sendFile('alprVideo.mp4', { root: 'public/uploads' });
+
 });
 
 
