e2ffe05cd3ed55d25dfee5995a51957dbdf6e4dc
Henok <henokmicrometre@gmail.com>
2023-09-02 13:41:59 +0100

    removing bloat

--- database.js
+++ database.js
@@ -5,26 +5,22 @@ const DBSOURCE = "db.sqlite"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
-      // Cannot open database
       console.error(err.message)
       throw err
     }else{
         console.log('Connected to the SQLite database.')
         db.run(`CREATE TABLE alpr (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            password text, 
             plate text, 
             uuid text
             )`,
         (err) => {
             if (err) {
-                // Table already created
             }else{
-                // Table just created, creating some rows
-                var insert = 'INSERT INTO alpr ( password, plate, uuid ) VALUES (?,?,?,?)'
-                db.run(insert, ["admin",md5("admin123456")])
-                db.run(insert, ["user",md5("user123456")])
-                db.run(insert, ["superuser",md5("user123456")])
+                var insert = 'INSERT INTO alpr ( plate, uuid ) VALUES (?,?,?,?)'
+                db.run(insert, ["admin"])
+                db.run(insert, ["user"])
+                db.run(insert, ["superuser"])
             }
         });  
     }
