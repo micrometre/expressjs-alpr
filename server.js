e6c0399218fe75802fc4d952deab0e4a5e02e887
Henok <henokmicrometre@gmail.com>
2023-09-06 00:31:55 +0100

    sorting

--- server.js
+++ server.js
@@ -24,19 +24,27 @@ const wss = new WebSocket.Server({ server })
 
 
 app.post("/anpr", (req, res, next) => {
-  plate = req.body.results[0].plate,
-  console.log(plate)
+  data = req.body.results[0].plate,
+  console.log(data)
 
   wss.on('connection', function connection(ws) {
     ws.on('message', function message(data) {
       console.log('received: %s', data);
     });
-    ws.send(plate);
-  });
-  res.send(plate)
+      var interval = setInterval(function () {
 
+    ws.send(data);
+
+  }, randomInteger(2, 9) * 1000);
+
+  });
+  res.send(data)
+  return
 })
 
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 app.get('/video', (req, res) => {
   res.sendFile('alprVideo.mp4', { root: 'public/uploads' });
--- wsserver.js
+++ wsserver.js
@@ -19,75 +19,30 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server })
 
 
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-  })
-  var interval = setInterval(function () {
-    db.all("SELECT * FROM alpr", function (err, rows) {
-      rows.forEach(function (row) {
-        data = JSON.stringify(row);
-        console.log(data)
-        ws.send(data)
-      });
-    });
-  }, randomInteger(2, 9) * 1000);
-  ws.on('close', function close() {
-    clearInterval(interval);
-  });
-})
 
+app.post("/anpr", (req, res, next) => {
+  data = req.body.results[0].plate,
+  console.log(data)
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+      console.log('received: %s', data);
+    });
+      var interval = setInterval(function () {
+    ws.send(data);
+  }, randomInteger(2, 9) * 1000);
+  });
+  res.send(data)
+  return
+})
 
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 app.get('/video', (req, res) => {
-  console.log(app.locals.alpr_data)
   res.sendFile('alprVideo.mp4', { root: 'public/uploads' });
-
 });
 
-app.post("/anpr", (req, res, next) => {
-  var data = {
-    plate: req.body.results[0].plate,
-    uuid: req.body.uuid,
-    confidence: req.body.results[0].confidence,
-    name: req.body.name,
-    email: req.body.email,
-  }
-  var sql = 'INSERT INTO alpr (plate, uuid ) VALUES (?,?)'
-  var params = [data.plate, data.uuid]
-  db.run(sql, params, function (err, result) {
-    if (err) {
-      res.status(400).json({ "error": err.message })
-      return;
-    }
-    res.json({
-      "message": "success",
-      "data": data,
-      "id": this.lastID
-    })
-  });
-})
-
-
-
-app.get("/api/alpr", (req, res, next) => {
-  var sql = "select * from alpr"
-  var params = []
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(400).json({ "error": err.message });
-      return;
-    }
-    res.json({
-      "message": "success",
-      "data": rows
-    })
-  });
-});
 
 server.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
