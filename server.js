4dcefb9c95aee26e9862f6a8a436dd051c9e9a52
Henok <henokmicrometre@gmail.com>
2023-09-05 13:14:17 +0100

    addinws

--- server.js
+++ server.js
@@ -9,30 +9,53 @@ app.use('/', express.static('public'));
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server })
 
+
+const db = require('./database.js')
+
+
+  newFunction()
+
+
+/*db.all("SELECT * FROM alpr", function(err, rows) {  
+  rows.forEach(function (row) {  
+      console.log(row);    // and other columns, if desired
+  })  
+});
+*/
+
 var data = "Real-Time Update 1";
 var number = 1;
-
 wss.on('connection', ws => {
-
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
   })
 
-  var interval = setInterval(function(){
-    data = "Real-Time Update "+number;
-    console.log("SENT: "+data);
+
+
+
+  var interval = setInterval(function () {
+    data = "Real-Time Update " + number;
+    console.log("SENT: " + data);
     ws.send(data)
     number++;
-  }, randomInteger(2,9)*1000);  
+  }, randomInteger(2, 9) * 1000);
 
   ws.on('close', function close() {
     clearInterval(interval);
   });
 })
 
+function newFunction() {
+  db.all("SELECT * FROM alpr", function (err, rows) {
+    rows.forEach(function (row) {
+      console.log(row)
+    })
+  })
+}
+
 function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}  
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 server.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
