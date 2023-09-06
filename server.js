0ef8bc3fe485bcefd1a78495c3348db96934f467
Henok <henokmicrometre@gmail.com>
2023-09-06 13:54:56 +0100

    sorting

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const http = require('http')
 const WebSocket = require('ws')
 const port = 5000
 const db = require('./database.js')
+const EventEmitter = require('node:events');
 
 
 app.use(cors()) // Use this after the variable declaration
@@ -14,36 +15,25 @@ app.use(bodyParser.json({ extended: true }));
 app.use('/', express.static('public'));
 
 
-
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server })
-
-
-
-
-
+wss.setMaxListeners(15)
 
 app.post("/anpr", (req, res, next) => {
   data = req.body.results[0].plate,
-    console.log(data)
-
+   // console.log(data)
   wss.on('connection', function connection(ws) {
+console.log(wss.getMaxListeners())
     ws.on('message', function message(data) {
       console.log('received: %s', data);
     });
-
     var interval = setInterval(function () {
-
       ws.send(data);
-
-    }, randomInteger(2, 9) * 9000);
-  ws.on('close', function close() {
-    clearInterval(interval);
+    }, randomInteger(2, 9) * 2000);
+    ws.on('close', function close() {
+      clearInterval(interval);
+    });
   });
-
-  });
-
-
   res.send(data)
   return
 })
--- wsserver.js
+++ wsserver.js
@@ -8,35 +8,28 @@ const WebSocket = require('ws')
 const port = 5000
 const db = require('./database.js')
 
-
 app.use(cors()) // Use this after the variable declaration
 app.use(bodyParser.json({ extended: true }));
 app.use('/', express.static('public'));
-
 
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server })
 
 
-
-
-
-
 app.post("/anpr", (req, res, next) => {
   data = req.body.results[0].plate,
-  console.log(data)
-
+    console.log(data)
   wss.on('connection', function connection(ws) {
     ws.on('message', function message(data) {
       console.log('received: %s', data);
     });
-      var interval = setInterval(function () {
-
-    ws.send(data);
-
-  }, randomInteger(2, 9) * 1000);
-
+    var interval = setInterval(function () {
+      ws.send(data);
+    }, randomInteger(2, 9) * 2000);
+    ws.on('close', function close() {
+      clearInterval(interval);
+    });
   });
   res.send(data)
   return
