436cd01894e393dea12cc12b0a8cecd5ad7a5875
Your Name <henokmicrometre@gmail.com>
2023-09-08 17:48:05 +0000

    fractiioning

---
+++ public/js/sse.js
@@ -0,0 +1,12 @@
+console.log('sse')
+    if (typeof (EventSource) !== "undefined") {
+      var source = new EventSource("/events");
+      source.onmessage = function (event) {
+        const box = document.getElementById('box')
+        const alpr = document.createElement('h1')
+        alpr.innerHTML += event.data.replace(/[{("")}]/g, '') + "<br>";
+        box.insertBefore(alpr, box.firstChild)
+      };
+    } else {
+      document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
+    }
