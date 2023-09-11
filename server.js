621faef8624e50957aca02549d9302f59430b09e
Henok <henokmicrometre@gmail.com>
2023-09-11 21:34:52 +0100

    sorting fs

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const serveIndex = require('serve-index')
 const path = require('path');
 const fs = require( 'fs' );
 
